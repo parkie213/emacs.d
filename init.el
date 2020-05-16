23b30bc05aea18931e68350e87632f4522cfa45f
James Parker <email goes here>
2020-05-16 14:28:48 +0100

    Moved to melpa

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("MELPA Stable" . "https://stable.melpa.org/packages/") t)
+             '("MELPA" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (when (not package-archive-contents)
